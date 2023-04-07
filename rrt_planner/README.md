4b326ff239447e77f3109fc1fb0cf2f5d2100123
Devansh Dhrafani <ddhrafan@andrew.cmu.edu>
2023-04-07 14:07:53 -0400

    Added sampling-based planners code

---
+++ rrt_planner/README.md
@@ -0,0 +1,11 @@
+# Sampling Based Planners
+
+This package contains relevant header and cpp files adapted from Devansh's Sampling based planner HW. The original code can be found [here](https://github.com/devanshdhrafani/sampling-based-planners). The following planners are available to use:
+
+- RRT
+- RRT-Connect
+- RRT*
+- PRM
+
+
+
---
+++ rrt_planner/include/rrt_planner/tree.h
@@ -0,0 +1,77 @@
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Tree{
+    public:
+        Tree(int nDOFs, double* armstart_anglesV_rad)
+        {
+            numDOFS = nDOFs;
+            nodes.clear();
+            edges.clear();
+            costs.clear();
+            nodes.push_back(armstart_anglesV_rad);
+            costs[0] = 0.0;
+        }
+
+        int addNode(double* sample_node_rad)
+        {
+            int node_id = nodes.size();
+            nodes.push_back(sample_node_rad);
+            return node_id;
+        }
+
+        void addEdge(int child, int parent)
+        {
+            edges[child] = parent;
+        }
+
+        double* getNode(int node_id)
+        {
+            return nodes[node_id];
+        }
+
+        int getParentID(int id)
+        {
+            return edges[id];
+        }
+
+        int getNewNodeID()
+        {
+            return nodes.size()-1;
+        }
+
+        int nearestNeighbour(double* node)
+        {
+            double min_dist = numeric_limits<double>::infinity();
+            int nearest_node_id = 0;
+            for (int i = 0; i < nodes.size(); i++) {
+                double dist = 0;
+                for (int j = 0; j < numDOFS; j++) {
+                    dist += (nodes[i][j] - node[j]) * (nodes[i][j] - node[j]);
+                }
+                dist = sqrt(dist);
+                if (dist < min_dist) {
+                    min_dist = dist;
+                    nearest_node_id = i;
+                }
+            }
+            return nearest_node_id;
+        }
+
+        double distBetweenNodes(double* node1, double* node2)
+        {
+            double dist = 0;
+            for (int i = 0; i < numDOFS; i++) {
+                dist += (node1[i] - node2[i]) * (node1[i] - node2[i]);
+            }
+            dist = sqrt(dist);
+            return dist;
+        }
+
+        vector<double*> nodes;
+        unordered_map<int, int> edges;    // child, parent
+        unordered_map<int, double> costs; // idx, cost
+        int numDOFS;
+};
