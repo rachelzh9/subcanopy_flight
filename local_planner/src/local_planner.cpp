95ba1887f518c27534277613ea4cc447c53d5b6e
Devansh Dhrafani <ddhrafan@andrew.cmu.edu>
2023-04-22 18:12:22 -0400

    Refactor, add support for 2D nav goal

--- local_planner/src/local_planner.cpp
+++ local_planner/src/local_planner.cpp
@@ -7,6 +7,7 @@
 #include <quadrotor_common/control_command.h>
 #include <quadrotor_common/geometry_eigen_conversions.h>
 #include <std_msgs/Bool.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <trajectory_generation_helper/heading_trajectory_helper.h>
 #include <trajectory_generation_helper/polynomial_trajectory_helper.h>
 #include <Eigen/Dense>
@@ -20,12 +21,18 @@ public:
 
     ros::Publisher arm_pub_;
 
-    ros::Subscriber autopilot_feedback_sub_;
+    ros::Subscriber goal_sub_;
 
     ros::ServiceClient planner_client;
 
     autopilot_helper::AutoPilotHelper autopilot_helper_;
     bool executing_trajectory_;
+
+    geometry_msgs::PoseStamped goal_pose_;
+    bool goal_received_;
+    bool goal_reached_;
+
+    polynomial_trajectories::PolynomialTrajectorySettings trajectory_settings;
 
     // Performance metrics variables
     double sum_position_error_squared_;
@@ -33,7 +40,9 @@ public:
     double sum_thrust_direction_error_squared_;
     double max_thrust_direction_error_;
 
-    // Constants
+    const double max_vel = 2.0;
+    const double max_thrust = 15.0;
+    const double max_roll_pitch_rate = 0.5;
     static constexpr double kExecLoopRate_ = 50.0;
 
     LocalPlanner()
@@ -45,129 +54,146 @@ public:
     {
 
         arm_pub_ = nh_.advertise<std_msgs::Bool>("bridge/arm", 1);
+        goal_sub_ = nh_.subscribe("/move_base_simple/goal", 1, &LocalPlanner::goalCallback, this);  // goal is in global namespace
         planner_client = nh_.serviceClient<rrt_planner::GetRRTPlan>("/rrt_planner_server");  // planner is in global namespace
-    }
 
-    ~LocalPlanner() {}
+        goal_received_ = false;
+        goal_reached_ = false;
 
-    void run()
-    {
-        ROS_INFO("Running trajectory node");
+        trajectory_settings.continuity_order = 4;
+        trajectory_settings.polynomial_order = 11;
 
-        ros::Rate command_rate(kExecLoopRate_);
-
-        // Make sure everything is up and running
-        // Wait for Autopilot feedback with assert
-        autopilot_helper_.waitForAutopilotFeedback(10.0, kExecLoopRate_);
+        if (!autopilot_helper_.waitForAutopilotFeedback(10.0, kExecLoopRate_))
+        {
+            ROS_ERROR("Autopilot feedback not received. Is the autopilot running?");
+        }
 
         ROS_INFO("Arming bridge");
-
-        // Arm bridge
         std_msgs::Bool arm_msg;
         arm_msg.data = true;
         arm_pub_.publish(arm_msg);
 
-        ///////////////
-        // Check off command
-        ///////////////
-
-        // Takeoff for real
+        // Takeoff
         autopilot_helper_.sendStart();
 
-        // Wait for autopilot to go to hover
         autopilot_helper_.waitForSpecificAutopilotState(autopilot::States::HOVER, 10.0, kExecLoopRate_);
+    }
 
-        // Generate trajectory, sample it and send it as reference states
-        const double max_vel = 2.0;
-        const double max_thrust = 15.0;
-        const double max_roll_pitch_rate = 0.5;
+    ~LocalPlanner() {}
 
-        quadrotor_common::TrajectoryPoint start_state;
-        start_state.position = autopilot_helper_.getCurrentReferenceState().position;
-        start_state.heading = 0.0;
+    void goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+    {
+        ROS_INFO("Goal received");
+        if (!goal_received_){
+            goal_received_ = true;
+        }
+        else{
+            ROS_INFO("Already have a goal. Will execute new goal when current goal is reached.");
+        }
+        goal_pose_ = *msg;
+    }
 
-        ROS_INFO("Starting trajectory control");
-
-        // Call RRT planner service
+    nav_msgs::Path getPlan(quadrotor_common::TrajectoryPoint start_state, geometry_msgs::PoseStamped goal_state)
+    {
         rrt_planner::GetRRTPlan planner_call = rrt_planner::GetRRTPlan();
         planner_call.request.start.pose.position.x = start_state.position(0);
         planner_call.request.start.pose.position.y = start_state.position(1);
 
-        // TODO: get goal from somewhere
-        planner_call.request.goal.pose.position.x = 7.0;
-        planner_call.request.goal.pose.position.y = 5.0;
+        planner_call.request.goal.pose.position.x = goal_state.pose.position.x;
+        planner_call.request.goal.pose.position.y = goal_state.pose.position.y;
 
-        // Send Obstacle IDs to avoid. Expects a int16[] array
+        // If empty, all obstacles are avoided
         std::vector<int16_t> obstacles = {};
         planner_call.request.obstacle_ids.data = obstacles;
 
         ros::service::waitForService("/rrt_planner_server");
-        if (!planner_client.call(planner_call))
-        {
+        if (!planner_client.call(planner_call)){
             ROS_ERROR("Failed to call planner service");
-            return;
+            return nav_msgs::Path();
         }
 
-        // Convert nav path to waypoints
-        int planlength = planner_call.response.plan.poses.size();
-        std::vector<Eigen::Vector3d> waypoints;
-        for (int i = 0; i < planlength; i++)
-        {
-            Eigen::Vector3d waypoint;
-            waypoint << planner_call.response.plan.poses[i].pose.position.x, planner_call.response.plan.poses[i].pose.position.y, start_state.position(2);
-            waypoints.push_back(waypoint);
-        }
+        return planner_call.response.plan;
+    }
 
-        // End state
+    void executeTrajectory(quadrotor_common::TrajectoryPoint start_state, nav_msgs::Path plan)
+    {
+        std::vector<Eigen::Vector3d> waypoints = pathToWaypoints(start_state, plan);
+
+        Eigen::VectorXd initial_segment_times = Eigen::VectorXd::Ones(int(waypoints.size())+1);
+        trajectory_settings.way_points = waypoints;
+
         quadrotor_common::TrajectoryPoint end_state;
         end_state.position = waypoints.back();
         end_state.heading = 0.0;
 
-        Eigen::VectorXd initial_segment_times =
-            Eigen::VectorXd::Ones(int(waypoints.size())+1);
-        polynomial_trajectories::PolynomialTrajectorySettings
-            trajectory_settings;
-        trajectory_settings.continuity_order = 4;
         Eigen::VectorXd minimization_weights(5);
         minimization_weights << 0.0, 1.0, 1.0, 1.0, 1.0;
         trajectory_settings.minimization_weights = minimization_weights;
-        trajectory_settings.polynomial_order = 11;
-        trajectory_settings.way_points = waypoints;
-
+        
         quadrotor_common::Trajectory traj = trajectory_generation_helper::
             polynomials::generateMinimumSnapTrajectory(
                 initial_segment_times, start_state, end_state, trajectory_settings, max_vel,
                 max_thrust, max_roll_pitch_rate, kExecLoopRate_);
-
-        // print trajectory before sending
-        // traj.points is a list
-        for(auto point : traj.points){
-            ROS_INFO("Trajectory point: %f, %f, %f", point.position(0), point.position(1), point.position(2));
-        }
-
+        
         autopilot_helper_.sendTrajectory(traj);
 
-        // Check if autopilot goes to trajectory control state
-        autopilot_helper_.waitForSpecificAutopilotState(autopilot::States::TRAJECTORY_CONTROL, 5.0, kExecLoopRate_);
+        goal_reached_ = true;
 
-        // Wait for autopilot to go back to hover
-        autopilot_helper_.waitForSpecificAutopilotState(autopilot::States::HOVER, 30.0, kExecLoopRate_);
-        ros::Duration(3.0).sleep();
+    }
 
-        ///////////////
-        // Check landing
-        ///////////////
+    std::vector<Eigen::Vector3d> pathToWaypoints(quadrotor_common::TrajectoryPoint start_state, nav_msgs::Path plan)
+    {
+        int planlength = plan.poses.size();
+        std::vector<Eigen::Vector3d> waypoints;
+        for (int i = 0; i < planlength; i++)
+        {
+            Eigen::Vector3d waypoint;
+            waypoint << plan.poses[i].pose.position.x, plan.poses[i].pose.position.y, start_state.position(2);
+            waypoints.push_back(waypoint);
+        }
+        return waypoints;
+    }
+
+    void run()
+    {
+        ros::Rate command_rate(kExecLoopRate_);
+
+        quadrotor_common::TrajectoryPoint start_state;
+        start_state.position = autopilot_helper_.getCurrentReferenceState().position;
+        start_state.heading = autopilot_helper_.getCurrentReferenceState().heading;
+
+        while (goal_received_)
+        {
+            if (goal_reached_){
+                goal_received_ = false;
+                goal_reached_ = false;
+                break;
+            }
+
+            ROS_INFO("Executing trajectory");
+            nav_msgs::Path plan = getPlan(start_state, goal_pose_);
+            executeTrajectory(start_state, plan);
+
+            autopilot_helper_.waitForSpecificAutopilotState(autopilot::States::TRAJECTORY_CONTROL, 5.0, kExecLoopRate_);
+
+            // Wait for autopilot to go back to hover
+            autopilot_helper_.waitForSpecificAutopilotState(autopilot::States::HOVER, 30.0, kExecLoopRate_);
+            ros::Duration(3.0).sleep();
+
+            ros::spinOnce();
+            command_rate.sleep();
+        }
 
         // Land
-        autopilot_helper_.sendLand();
+        // autopilot_helper_.sendLand();
 
-        // Wait for autopilot to go to land
-        autopilot_helper_.waitForSpecificAutopilotState(autopilot::States::LAND, 10.0, kExecLoopRate_);
+        // // Wait for autopilot to go to land
+        // autopilot_helper_.waitForSpecificAutopilotState(autopilot::States::LAND, 10.0, kExecLoopRate_);
 
-        // Wait for autopilot to go to off
-        autopilot_helper_.waitForSpecificAutopilotState(autopilot::States::OFF, 10.0, kExecLoopRate_);
+        // // Wait for autopilot to go to off
+        // autopilot_helper_.waitForSpecificAutopilotState(autopilot::States::OFF, 10.0, kExecLoopRate_);
 
-        autopilot_helper_.sendOff();
+        // autopilot_helper_.sendOff();
     }
 };
 
@@ -176,7 +202,10 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "local_planner");
     LocalPlanner planner;
-    planner.run();
-    ros::spin();
+
+    while(ros::ok()){
+        planner.run();
+        ros::spinOnce();
+    }
     return 0;
 }
